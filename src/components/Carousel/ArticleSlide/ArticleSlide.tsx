97d378b73a5d423cb772590d7d32000d9d6cf045
Mauzek <artemilliy@gmail.com>
2025-06-24 00:09:35 +0300

    refactor(articleSlide): добавь обработку внешних ссылок соблюдая семантику

--- src/components/Carousel/ArticleSlide/ArticleSlide.tsx
+++ src/components/Carousel/ArticleSlide/ArticleSlide.tsx
@@ -7,48 +7,67 @@ export const ArticleSlide: React.FC<ArticleSlideProps> = ({
   slide,
   isActive,
 }) => {
-  const linkTo = slide.link ? `/games/${slide.gameName.toLowerCase().replace(/\s+/g, "_").replace(/:/g, "")}` : '/';
+  const isExternalLink = slide.link && slide.link.startsWith("https");
+  const linkTo = !isExternalLink ? `/games/${slide.gameName.toLowerCase().replace(/\s+/g, "_").replace(/:/g, "")}` : slide.link;
+  
+  const linkContent = (
+    <>
+      <div className={styles["slide__image-container"]}>
+        <picture className={styles.slide__picture}>
+          <source srcSet={slide.imageMobile} media="(max-width: 640px)" />
+          <img
+            src={slide.imagePC}
+            alt={slide.title}
+            className={styles.slide__image}
+            decoding="auto"
+          />
+        </picture>
+      </div>
+
+      <div className={styles.slide__content}>
+        <div className={styles["slide__badges-container"]}>
+          <span className={styles.slide__badge}>{slide.gameName}</span>
+          {slide.badge && (
+            <span className={styles.slide__badge}>{slide.badge}</span>
+          )}
+        </div>
+        <h3 className={styles.slide__title}>{slide.title}</h3>
+
+        {slide.description && (
+          <div className={styles.slide__description}>
+            <div
+              className={styles["slide__description-body"]}
+              dangerouslySetInnerHTML={{ __html: slide.description.body }}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+  
   return (
     <div
       role="group"
       aria-roledescription="slide"
       className={`${styles.slide} ${isActive ? styles["slide--active"] : ""}`}
     >
-      <Link
-        to={linkTo}
-        className={styles.slide__link}
-      >
-        <div className={styles["slide__image-container"]}>
-          <picture className={styles.slide__picture}>
-            <source srcSet={slide.imageMobile} media="(max-width: 640px)" />
-            <img
-              src={slide.imagePC}
-              alt={slide.title}
-              className={styles.slide__image}
-              decoding="auto"
-            />
-          </picture>
-        </div>
-
-        <div className={styles.slide__content}>
-          <div className={styles["slide__badges-container"]}>
-            <span className={styles.slide__badge}>{slide.gameName}</span>
-            {slide.badge && (
-              <span className={styles.slide__badge}>{slide.badge}</span>
-            )}
-          </div>
-          <h3 className={styles.slide__title}>{slide.title}</h3>
-
-          {slide.description && (
-            <div className={styles.slide__description}>
-              <div
-                className={styles["slide__description-body"]}
-                dangerouslySetInnerHTML={{ __html: slide.description.body }}
-              />
-            </div>
-          )}
-        </div>
-      </Link>
+      {isExternalLink ? (
+        <a
+          href={linkTo}
+          className={styles.slide__link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {linkContent}
+        </a>
+      ) : (
+        <Link
+          to={linkTo}
+          className={styles.slide__link}
+        >
+          {linkContent}
+        </Link>
+      )}
     </div>
   );
 };
