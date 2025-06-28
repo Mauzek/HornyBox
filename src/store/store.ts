05a7377dc1de4946c96c2742bae17a05de6f1517
Mauzek <artemilliy@gmail.com>
2025-06-28 17:22:54 +0300

    feat(store): добавить cardSlice в store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { assetsApi, productsApi } from "./apiQueries";
-import { userSlice } from "./slices";
+import { userSlice, cartSlice } from "./slices";
 
 export const store = configureStore({
   reducer: {
     [assetsApi.reducerPath]: assetsApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
     user: userSlice.reducer,
+    cart: cartSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(assetsApi.middleware, productsApi.middleware),
