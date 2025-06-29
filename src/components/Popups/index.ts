648319f5b73f653da5ef76608b5295021632581b
Mauzek <artemilliy@gmail.com>
2025-06-29 18:52:30 +0300

    styles(index): добавить стили для ссылок и кнопок в index.scss

--- src/components/Popups/index.ts
+++ src/components/Popups/index.ts
@@ -1,3 +1,4 @@
 export { OrderHistory } from "./OrderHistory";
 export { ActionMenu } from "./ActionMenu";
 export { VideoPlayer } from "./VideoPlayer";
+export { ProductCardInfo } from "./ProductCardInfo";
