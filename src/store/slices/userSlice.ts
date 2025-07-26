d0e0eada71a5358dd7219ff25ff77cc4eec3bb99
Mauzek <artemilliy@gmail.com>
2025-07-26 21:45:57 +0300

    feat(userSlice): доработать userSlice

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,18 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { UserData } from "../../types";
-import { loadFromLocalStorage } from "../../utils";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { User, UserState } from "../../types";
 
-const initialState: UserData = loadFromLocalStorage("user");
+const initialState: UserState = {
+  user: null,
+  isAuthenticated: false,
+  loading: true, 
+};
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action) => {
+    setUser: (state, action: PayloadAction<User | null>) => {
       state.user = action.payload;
-      localStorage.setItem("user", JSON.stringify(action.payload));
+      state.isAuthenticated = !!action.payload;
+      state.loading = false;
+    },
+    
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    
+    clearUser: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+      state.loading = false;
     },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setLoading, clearUser } = userSlice.actions;
+
+export default userSlice.reducer;
