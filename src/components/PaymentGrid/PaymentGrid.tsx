9f7451d6bd3ac389b4ca947dc015b3371255587a
Mauzek <artemilliy@gmail.com>
2025-07-31 23:27:17 +0300

    feat(paymentGrid): добавить просы для стилизации className и style

--- src/components/PaymentGrid/PaymentGrid.tsx
+++ src/components/PaymentGrid/PaymentGrid.tsx
@@ -7,9 +7,11 @@ export const PaymentGrid: React.FC<PaymentGridProps> = ({
   payments,
   selectedPayment,
   setSelectedPayment,
+  className,
+  style
 }) => {
   return (
-    <section className={styles.payment_grid}>
+    <section className={`${styles.payment_grid} ${className}`} style={style} >
       {payments.map((method) => (
         <PaymentMethod
           key={method.id}
--- src/components/PaymentGrid/types.ts
+++ src/components/PaymentGrid/types.ts
@@ -4,4 +4,6 @@ export interface PaymentGridProps{
     payments: Payment[];
     selectedPayment: number;
     setSelectedPayment: (paymentId: number) => void;
+    className?: string,
+    style?: Record<string, string>
 }
