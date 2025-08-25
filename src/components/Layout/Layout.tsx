11fb4f1159d6480a35f9912ef043f523077e4d0d
Artyom Illiy <artemilliy@gmail.com>
2025-08-25 17:57:19 +0300

    feat(vercelAnalitics): добавить ссылку на демо и аналитику от vercel

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,35 +4,33 @@ import { useGetAssetsQuery } from "../../store";
 import styles from "./Layout.module.scss";
 import { usePreloader } from "../../hooks/usePreloader";
 import { useFirebase } from "../../hooks";
+import { Analytics } from "@vercel/analytics/next";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const { data, isLoading, isError } = useGetAssetsQuery();
-  const {shouldShowPreloader, handlePreloaderComplete} = usePreloader({
+  const { shouldShowPreloader, handlePreloaderComplete } = usePreloader({
     isLoading,
   });
   useFirebase();
 
   if (!data || isError) {
     return (
-      <Preloader
-        isLoading={isLoading}
-        onComplete={handlePreloaderComplete}
-      />
+      <Preloader isLoading={isLoading} onComplete={handlePreloaderComplete} />
     );
   }
 
   return (
     <>
       {shouldShowPreloader && (
-        <Preloader
-          isLoading={isLoading}
-          onComplete={handlePreloaderComplete}
-        />
+        <Preloader isLoading={isLoading} onComplete={handlePreloaderComplete} />
       )}
 
       <ScrollToTop />
       <Header />
-      <main className={styles.main}>{children}</main>
+      <main className={styles.main}>
+        {children} 
+        <Analytics />
+      </main>
       <TabBar />
       <Footer />
     </>
